b15d72172a0f7b7e3579b92309037fa8e9f5eef3
Tiffany <191784353+jstiffany@users.noreply.github.com>
2026-08-01 01:36:27 +0000

    might just restart : its late and i 100% screwed up

--- expenses_tracker.py
+++ expenses_tracker.py
@@ -5,6 +5,10 @@ expenses_log = {
 }
 
 def add_expenses(expense, price):
+    if expense in expenses_log:
+        print("Expense already in log!")
+        return
+
     print("\nAdding expense...")
 
     if price < 0:
@@ -15,7 +19,6 @@ def add_expenses(expense, price):
     print("\nExpense added!")
 
 def remove_expense(expense):
-    # easier way
     del_expense = expenses_log.pop(expense, "Item not found!")
 
     if del_expense == "Item not found!":
@@ -26,9 +29,54 @@ def remove_expense(expense):
 
     return
 
-def edit_expense(expense, type):
-    if type == "1":
-        pass
+def edit_expense(expense, update_type):
+    if not expenses_log:
+        print("\nNo expenses found.")
+        return
+
+    if expense not in expenses_log:
+        print("\nExpense not found.")
+        return
+
+    print("\nExpense found!")
+
+    if update_type == 1:
+        new_name = input("\nWhat is the new name for this expense? ")
+        old_price = expenses_log[expense]["price"]
+
+        del expenses_log[expense]
+
+        expenses_log[new_name] = {"price": old_price}
+        print("\nName updated!")
+
+    else:
+        if expense in expenses_log:
+            print("\nExpense found!")
+
+            new_name = input("\nWhat is the new name for this expense?")
+
+            expenses_log[expense] = new_name
+            print("\nName updated!")
+
+        elif type == 2:
+            if not expenses_log:
+                print("\nNo expenses found.")
+                return
+
+        else:
+            print("Searching for expense...")
+
+            if expense in expenses_log:
+                print("Expense found!")
+
+                new_price = float(input("\nWhat is the new price of this expense? "))
+
+                expenses_log[expense]["price"] = new_price
+                print("\nPrice updated!")
+
+            else:
+                print("Thats not a value.")
+                return
 
 def search_expense(expense):
     if not expenses_log:
@@ -39,19 +87,19 @@ def search_expense(expense):
 
         if expense in expenses_log:
             print("Expense found!\n")
-            print(expenses_log[expense])
+            print(f"{expense}: ${expenses_log[expense]['price']}")
 
         else:
             print("\nExpense not found.")
 
 def sum_expenses():
     if expenses_log:
-        total_sum = sum([item["price"] for item in expenses_log.values()])
+        total_sum = sum(item["price"] for item in expenses_log.values())
 
         print("\n------- Expenses -------\n")
         for i, item in enumerate(expenses_log, 1):
             price = expenses_log[item]["price"]
-            print(f"\n{i}: {item} ; {price}")
+            print(f"\n{i}: {item} ; ${price}")
 
         print("\n-----------------------------------")
         print(f"Total expenses: ${total_sum:.2f}")
@@ -61,7 +109,44 @@ def sum_expenses():
 
 while True:
     try:
-        menuopt = int(input("How would you like to manage your expense tracker?\n1."))
+        menuopt = int(input("\nHow would you like to manage your expense tracker?\n1. Add expense\n2. Update expense\n3. Remove expense\n4. Sum expenses\n5. Search/display expeses\n6. Exit\n"))
+
+        if menuopt == 1:
+            add_expense = input("\nWhat is the name of this expense? ")
+            expense_price = float(input("What is the price of this expense? "))
+
+            add_expenses(add_expense, expense_price)
+
+        elif menuopt == 2:
+            find_expense = input("\nWhat is the name of this expense? ")
+            update_type = int(input("\nWould you either like to:\n1. Update name\n2. Update price\n"))
+
+            edit_expense(find_expense, update_type)
+
+        elif menuopt == 3:
+            rem_expense = input("\nWhat is the name of this expense? ")
+
+            remove_expense(rem_expense)
+
+        elif menuopt == 4:
+            sum_expenses()
+
+        elif menuopt == 5:
+            search_type = int(input("\nDo you want to:\n1. Search for an expense\n2. Display all expesnses\n"))
+
+            if search_type == 1:
+                expense_search = input("What is the expense you are searching for? ")
+                search_expense(expense_search)
+
+            elif search_type == 2:
+                sum_expenses()
+
+            else:
+                print("Thats not a value.")
+
+        elif menuopt == 6:
+            print("\nExiting...")
+            break
 
 
     except ValueError:
